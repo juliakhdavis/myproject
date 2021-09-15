63622ec8248be3a76dc2700f6dd9b6fceda4dc47
juliakhdavis <juliakhdavis@gmail.com>
2021-09-14 19:26:31 -0700

    commit

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-let pugphoto = 0;
+let derreklewis = 0;
 
 function movePug(){
     var app = document.getElementsByClassName('pugphoto');
 
-    if(pugphoto == 100){
-        pugphoto = 0;
+    if(derreklewis == 100){
+        derreklewis = 0;
     }
 
     for(let i = 0; i < app.length; i++){
-        app[i].style.left = pugphoto + '%';
-        console.log(pugphoto);
+        app[i].style.left = derreklewis + '%';
+        console.log(derreklewis);
     }
-    pugphoto++;
+    derreklewis++;
 }
