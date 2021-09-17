275a09454cccc843cd17c52fe13237c46197fec1
Sbollman011 <58572531+Sbollman011@users.noreply.github.com>
2021-09-17 12:47:42 -0700

    Simplified movePug() in main.js

--- main.js
+++ main.js
@@ -1,15 +1,7 @@
 let derreklewis = 0;
 
 function movePug(){
-    var app = document.getElementsByClassName('derreklewis');
-
-    if(derreklewis == 100){
-        derreklewis = 0;
-    }
-
-    for(let i = 0; i < app.length; i++){
-        app[i].style.left = derreklewis + '%';
-        console.log(derreklewis);
-    }
-    derreklewis++;
+    var app = document.querySelector('.derrecklewis');
+        app.style.left = derreklewis + '%';  
+        derreklewis = derreklewis+1;
 }
